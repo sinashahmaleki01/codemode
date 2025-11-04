2a2d910dfcd912c9ed55bc92ec3ee7fc9bafd9c5
AMIRH03EIN <amirh03einxzx@gmail.com>
2025-11-04 16:55:35 +0330

    Add autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['last 2 versions', '> 1%'],
+        }),
+      ],
+    },
+  },
+});
+
